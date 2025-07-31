571098d1a2bea97fadb66607f0f2d16e26e23447
surendradire <surendra.dire@gmail.com>
2025-07-31 22:01:46 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1 style={{ fontSize: "48px", color: "#2e3b4e" }}>
+        🌊 This is the <strong>Blue</strong> version!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
